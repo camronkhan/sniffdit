30f93602f5751f9fad7ad9dbed4c1ec12ec0b069
Camron Khan <camron.khan@gmail.com>
2016-02-03 11:56:39 -0600

    added bower components

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET all posts */
+router.get('/posts', function(req, res, next) {
+	Post.find(function(err, posts) {
+		if (err) {
+			return res.send(500, 'Error: No data to return');
+		}
+		return res.json(posts);
+	});
 });
 
 module.exports = router;
