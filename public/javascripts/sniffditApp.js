157b00d0d8d475bd448ecafac431e54cdc73a6e9
Camron Khan <camron.khan@gmail.com>
2016-02-03 12:10:07 -0600

    updated routes/api, added client app

---
+++ public/javascripts/sniffditApp.js
@@ -0,0 +1,36 @@
+var app = angular.module('sniffditApp', ['ngRoute', 'ngResource']).run(function($http, $rootScope) {
+});
+
+// Configure routing for views and controllers
+app.config(function($routeProvider) {
+	$routeProvider
+		// Content display
+		.when('/', {
+			templateUrl: 'main.html',
+			controller: 'mainController'
+		});
+});
+
+app.factory('postService', function($resource) {
+	return $resource('/api/posts/:id')
+});
+
+app.controller('mainController', function($scope, postService) {
+	$scope.posts = postService.query();
+	$scope.newPost = { title: '', url: '', img: '', posted_by: '', posted_on: '' };
+
+	$scope.post = function(){
+		$scope.newPost.posted_by = $rootScope.current_user;
+		$scope.newPost.posted_on = Date.now();
+		postService.save($scope.newPost, function() {
+			$scope.posts = postService.query();
+			$scope.newPost = { title: '', url: '', img: '', posted_by: '' };
+		});
+	};
+
+	$scope.delete = function(post) {
+		postService.delete({id: post._id});
+		$scope.posts = postService.query();
+	};
+
+});
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require( 'mongoose' );
+var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 
 /* GET all posts */
@@ -10,6 +10,72 @@ router.get('/posts', function(req, res, next) {
 			return res.send(500, 'Error: No data to return');
 		}
 		return res.json(posts);
+	});
+});
+
+/* GET block of posts */
+/*router.get('/posts', function(req, res, next) {
+	// TODO
+  	res.send({message: 'GET block of posts'});
+});
+
+/* GET post by id */
+router.get('/posts/:id', function(req, res, next) {
+	Post.findById(req.params.id, function(err, post) {
+		if (err) {
+			res.send(err);
+		}
+		res.json(post);
+	});
+});
+
+/* POST */
+router.post('/posts', function(req, res, next) {
+	// Declare a new post obj
+	var post = new Post({
+		'title': req.body.title,
+		'url': req.body.url,
+		'img': req.body.img,
+		'posted_by': req.body.posted_by
+	});
+
+	// Insert post into db
+	post.save(function(err, post) {
+		if (err) {
+			return res.send(500, 'Error: Post was not saved');
+		}
+		return res.json(post);
+	});
+});
+
+/* PUT */
+router.put('/posts/:id', function(req, res, next) {
+	Post.findById(req.params.id, function(err, post) {
+		if (err) {
+			res.send(err);
+		}
+		// Assign new values
+		post.title = req.body.title;
+		post.url = req.body.url;
+		post.img = req.body.img;
+		post.posted_by = req.body.posted_by;
+		// Save new values to post
+		post.save(function(err, post) {
+			if (err) {
+				res.send(err);
+			}
+			res.json(post);
+		});
+	});
+});
+
+/* DELETE */
+router.delete('/posts/:id', function(req, res, next) {
+	Post.remove({ _id: req.params.id }, function(err, results) {
+		if (err) {
+			res.send(err);
+		}
+		res.json("Post deleted");
 	});
 });
 
